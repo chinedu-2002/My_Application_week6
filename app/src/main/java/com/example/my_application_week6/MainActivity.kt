8536e96f51618b4907805ea93b54f594a59b021b
Emmanuel Enenta <ecenenta16@my.fisk.edu>
2025-07-14 20:02:18 -0500

    new

--- app/src/main/java/com/example/my_application_week6/MainActivity.kt
+++ app/src/main/java/com/example/my_application_week6/MainActivity.kt
@@ -1,20 +1,48 @@
 package com.example.my_application_week6
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.codepath.asynchttpclient.AsyncHttpClient
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import okhttp3.Headers
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        fetchHeroesImage()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
     }
+    private fun fetchHeroesImage() {
+        val client = AsyncHttpClient()
+        var choice = Random.nextInt(60)
+        client["https://pokeapi.co/api/v2/pokemon/$choice", object : JsonHttpResponseHandler() {
+            override fun onSuccess(statusCode: Int, headers: Headers, json: JsonHttpResponseHandler.JSON) {
+                Log.d("Heroes", "response successful")
+            }
+
+            override fun onFailure(
+                statusCode: Int,
+                headers: Headers?,
+                errorResponse: String,
+                throwable: Throwable?
+            ) {
+                Log.d("Heroes Error", errorResponse)
+            }
+        }]
+
+
+    }
+
+
 }
